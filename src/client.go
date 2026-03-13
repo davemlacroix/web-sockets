8e286cc63b2b0538ca221306f660f3100ac8c602
davemlacroix <davemlacroix@gmail.com>
2026-03-13 13:54:19 -0400

    move client implementation into separate file

---
+++ src/client.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+)
+
+type TCPClient struct {
+	addr string
+	conn net.Conn
+}
+
+type Client interface {
+	Connect() error
+	Send(message []byte) error
+	Close() error
+}
+
+func NewClient(addr string) *TCPClient {
+	return &TCPClient{
+		addr: addr,
+	}
+}
+
+func (c *TCPClient) Connect() error {
+	conn, err := net.Dial("tcp", "127.0.0.1:9001")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("error with initial connection")
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	req, err := http.NewRequest("GET", "http://127.0.0.1:9001/getCaseCount", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("Upgrade", "websocket")
+	req.Header.Add("Connection", "upgrade")
+	req.Header.Add("Upgrade", "websocket")
+	req.Header.Add("Sec-WebSocket-Key", "dGhlIHNhbXBsZSBub25jZQ==")
+	req.Header.Add("Origin", "127.0.0.1")
+	req.Header.Add("Sec-WebSocket-Protocol", "chat, superchat")
+	req.Header.Add("Sec-WebSocket-Version", "13")
+
+	err = req.Write(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(conn)
+	resp, err := http.ReadResponse(reader, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	buf := make([]byte, 1024)
+	io.ReadFull(reader, buf)
+	fmt.Println(buf)
+}
--- src/websocket.go
+++ src/websocket.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"log"
-	"net"
-	"net/http"
 )
 
 func main() {
@@ -14,40 +9,6 @@ func main() {
 	//get test cases count by connecting to:
 	//ws://localhost:9001/getCaseCount
 
-	conn, err := net.Dial("tcp", "127.0.0.1:9001")
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println("error with initial connection")
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	req, err := http.NewRequest("GET", "http://127.0.0.1:9001/getCaseCount", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Add("Upgrade", "websocket")
-	req.Header.Add("Connection", "upgrade")
-	req.Header.Add("Upgrade", "websocket")
-	req.Header.Add("Sec-WebSocket-Key", "dGhlIHNhbXBsZSBub25jZQ==")
-	req.Header.Add("Origin", "127.0.0.1")
-	req.Header.Add("Sec-WebSocket-Protocol", "chat, superchat")
-	req.Header.Add("Sec-WebSocket-Version", "13")
-
-	err = req.Write(conn)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	reader := bufio.NewReader(conn)
-	resp, err := http.ReadResponse(reader, req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	buf := make([]byte, 1024)
-	io.ReadFull(reader, buf)
-	fmt.Println(buf)
+	c := NewClient("127.0.0.1:9001")
+	c.Connect()
 }
